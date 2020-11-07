93f36286595cc6280171d27c8fd8c4006675691c
jokerGit-Pro <goodMorning@joker.com>
2020-11-07 22:14:28 +0800

    服务提供方执行降级方法

--- cloud-provider-hystrix-payment8001/src/main/java/com/joker/cloud/hystrix/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/joker/cloud/hystrix/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.joker.cloud.hystrix.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -16,12 +18,25 @@ public class PaymentService {
         return "当前·线程："+Thread.currentThread().getName()+" 执行OK";
     }
 
+    /**
+     *  execution.isolation.thread.timeoutInMilliseconds 3000：规定了该业务方法的执行时间，超时就会执行降级方法
+     *  如果在3000毫秒内执行方法时遇到异常，直接就会执行降级方法
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "payment_TimeOout_Handler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String payment_TimeOout(){
         try{
-            TimeUnit.SECONDS.sleep(3);
+            //int age=10/0;
+            TimeUnit.SECONDS.sleep(5);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
         return "当前 线程："+Thread.currentThread().getName()+" 执行超时";
+    }
+
+    public String payment_TimeOout_Handler(){
+        return "当前 线程："+Thread.currentThread().getName()+" 由于超时，执行了降级方法";
     }
 }
